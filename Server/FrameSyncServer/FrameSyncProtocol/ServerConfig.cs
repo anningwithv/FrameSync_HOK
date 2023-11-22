07480c9e69b6f69d53dece3a32a324b57aef54fe
v <378294113@qq.com>
2023-11-22 09:02:24 +0800

    merge

--- Server/FrameSyncServer/FrameSyncProtocol/ServerConfig.cs
+++ Server/FrameSyncServer/FrameSyncProtocol/ServerConfig.cs
@@ -4,12 +4,7 @@ namespace FrameSyncProtocol
 {
     public class ServerConfig
     {
-        public const string LocalDevInnerIP = "192.168.26.173";
-        public const int UdpPort = 17666;
-
-        //确认匹配倒计时：15秒
-        public const int ConfirmCountDown = 15;
-        //选择英雄倒计时：15秒
-        public const int SelectCountDown = 15;
+        public const string LocalDevInnerIP = "192.168.3.208";
+        public const int UdpPort = 8888;
     }
 }
